ae1976ee52879ddb910ceb2d88e8b9fb634c454a
Sanket Maheshwari <sanket@Sankets-MacBook-Pro.local>
2021-07-08 00:11:00 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from os import name
+from setuptools import setup, find_packages, version
+
+setup (
+    name= "src",
+    version= "0.0.1",
+    description= "it's a wine Quality package",
+    author= "Sanket",
+    packages= find_packages(),
+    license= "MIT"
+    )
