e117d7a58092e2938c8c2de361806a60942fcec4
Sanket Maheshwari <sanket@Sankets-MacBook-Pro.local>
2021-07-12 13:53:07 +0530

    pytest example

--- tests/test_config.py
+++ tests/test_config.py
@@ -13,3 +13,16 @@ def test_generic():
     with pytest.raises(NotInRange):
         if a not in range(10, 20):
             raise NotInRange
+
+
+# Its a convention, it's compulsory to include test_XXX in from of any function, otherwise it won't consider it as a test case
+
+def something():
+    a = 5
+    b = 5
+    assert True
+
+def test_something():
+    a = 5
+    b = 5
+    assert True
